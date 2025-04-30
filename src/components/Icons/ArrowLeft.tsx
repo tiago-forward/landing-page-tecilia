442d30ba9056bc2e6617bb4a37b61748f7a636eb
tiago-forward <devtiagoforward@gmail.com>
2025-04-30 13:03:52 -0300

    fix svg

--- src/components/Icons/ArrowLeft.tsx
+++ src/components/Icons/ArrowLeft.tsx
@@ -7,9 +7,9 @@ export default function ArrowLeft() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-arrow-big-left-icon lucide-arrow-big-left"
     >
       <path d="M18 15h-6v4l-7-7 7-7v4h6v6z" />
--- src/components/Icons/ArrowRight.tsx
+++ src/components/Icons/ArrowRight.tsx
@@ -7,9 +7,9 @@ export default function ArrowRight() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-arrow-big-right-icon lucide-arrow-big-right"
     >
       <path d="M6 9h6V5l7 7-7 7v-4H6V9z" />
